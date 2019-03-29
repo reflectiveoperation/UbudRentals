70e6e4936fa384ecb2fff69230e9a136d2507dad
Cristian Perez <cristian.perez2011@gmail.com>
2019-03-29 17:20:41 +0800

    Completed full implementation

--- src/main/java/com/cristianperez/ubudrentalwithspring/logic/configuration/SecurityConfiguration.java
+++ src/main/java/com/cristianperez/ubudrentalwithspring/logic/configuration/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/index","/movieSearchResult")
+                .antMatchers("/","/index","/movie-search-result","/register")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
