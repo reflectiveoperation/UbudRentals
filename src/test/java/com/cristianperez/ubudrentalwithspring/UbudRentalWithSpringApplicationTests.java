1d138aea13e3afc17bc2c9ea093897cef9257df2
Cristian Perez <cristian.perez2011@gmail.com>
2019-04-03 15:49:55 +0800

    added silly test

--- src/test/java/com/cristianperez/ubudrentalwithspring/UbudRentalWithSpringApplicationTests.java
+++ src/test/java/com/cristianperez/ubudrentalwithspring/UbudRentalWithSpringApplicationTests.java
@@ -3,11 +3,14 @@ package com.cristianperez.ubudrentalwithspring;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 
 public class UbudRentalWithSpringApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertThat(1).isEqualTo(1);
 	}
 
 }
